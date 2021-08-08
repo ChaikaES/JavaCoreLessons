ebbe1080509af4695c30638b62b7c29083990b16
Chaika <chaikaes@gmail.com>
2021-08-08 20:56:20 +0300

    Done

--- src/main/java/ru/gb/Lesson_2/App.java
+++ src/main/java/ru/gb/Lesson_2/App.java
@@ -2,15 +2,42 @@ package ru.gb.Lesson_2;
 
 public class App {
 
-    public static void main(String[] args) throws MyArraySizeException {
-        String[][] arr = new String[4][4];
+    public static void main(String[] args) {
+        String[][] arr = {
+                {"1","2","3","4"},
+                {"5","6","n","8"},
+                {"9","12","13","14"},
+                {"11","21","31","41"}
+        };
 
-        run(arr);
+        try {
+            int result = run(arr);
+            System.out.println(result);
+        } catch (MyArrayDataException e) {
+            System.out.println(e.toString());
+        } catch (MyArraySizeException e) {
+            System.out.println("Неправильный размер массива");
+        }
+
     }
 
-    public static void run(String[][] arr) throws MyArraySizeException {
+    public static int run(String[][] arr) throws MyArrayDataException, MyArraySizeException {
         if (arr.length != 4 || arr[0].length != 4) {
             throw new MyArraySizeException();
         }
+
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++){
+                try {
+                    int value = Integer.parseInt(arr[i][j]);
+                    sum = sum + value;
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i + 1, j + 1, arr[i][j]);
+                }
+            }
+        }
+
+        return sum;
     }
 }
--- src/main/java/ru/gb/Lesson_2/MyArrayDataException.java
+++ src/main/java/ru/gb/Lesson_2/MyArrayDataException.java
@@ -1,4 +1,17 @@
 package ru.gb.Lesson_2;
 
-public class MyArrayDataException {
+public class MyArrayDataException extends Exception {
+    int row;
+    int col;
+    String value;
+
+    MyArrayDataException (int row, int col, String value) {
+        this.row = row;
+        this.col = col;
+        this.value = value;
+    }
+
+    public String toString() {
+        return String.format("Ошибка в строке %s колонке %s. Значение в ячейке \"%s\"", row, col, value);
+    }
 }
